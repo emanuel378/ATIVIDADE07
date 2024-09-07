37aa96a3b05926ba8394280415b60bf301b90542
emanuel378 <emanuelmvieira10@gmail.com>
2024-09-07 18:21:56 -0300

    Imposto

---
+++ atv07.py
@@ -0,0 +1,4 @@
+# Enunciado: Uma loja aplica um imposto de 12% sobre o valor dos produtos. Crie um programa que receba o preço de um produto e calcule o preço final com o imposto incluído.
+produto = int(input("Digite o preço do seu produto:"))
+imposto = produto * 1.12
+print(f"O valor final com imposto de 12% ficou: {imposto}")
